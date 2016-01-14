1802977d801c3f4e3b63347431a8c3a8ee888714
Christi John <Christi@Christis-MacBook-Pro-2.local>
2016-01-14 17:52:10 +0530

    completed implementation

--- UITextfield Addition/Uitextfield_Addition/Uitextfield_Addition/ViewController.swift
+++ UITextfield Addition/Uitextfield_Addition/Uitextfield_Addition/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextFied: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        firstTextField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sumAction(sender: AnyObject) {
+        
+        firstTextField.resignFirstResponder()
+        secondTextFied.resignFirstResponder()
+        
+        let number1        = Int (firstTextField.text!)
+        let number2        = Int (secondTextFied.text!)
+        let result         = number1! + number2!
+        resultLabel.text   = String(result);
+        
+    }
 
+    @IBAction func resetAction(sender: AnyObject) {
+        
+        firstTextField.text = ""
+        secondTextFied.text = ""
+        resultLabel.text    = ""
+        firstTextField.becomeFirstResponder()
+    }
 }
 
